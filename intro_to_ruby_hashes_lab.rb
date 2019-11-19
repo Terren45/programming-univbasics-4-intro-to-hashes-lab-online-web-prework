11d6381ffd327253ceb9f080907a704ff40257bd
Terren Culberson <culbersonterren@yahoo.com>
2019-11-19 04:24:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value )
-  my_hash_creator = {:hats => "shorts"}
+  my_hash_creator = {:hats => "shorts", :shoes => "socks"}
 end
 
 def read_from_hash(hash, key)
